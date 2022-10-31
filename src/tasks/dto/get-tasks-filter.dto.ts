000bd61e209d8455c2934eb7e2eda0c0f44fa33a
takashi247 <takashi247@gmail.com>
2022-10-31 22:30:21 +0900

    complete section 3

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status?: TaskStatus;
+
+  @IsString()
+  @IsOptional()
   search?: string;
 }
